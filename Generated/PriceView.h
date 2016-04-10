eb154c0268662a5b1f469a92dc23d6e9a3852cfe
Peter Wiesner <peter.wiesner@distinction.hu>
2016-04-10 17:36:10 +0200

    Generating iVars
    Traversing layer hierarchy, JS-style string concatenation

--- Generated/PriceView.h
+++ Generated/PriceView.h
@@ -1,6 +1,11 @@
 #import <UIKit/UIKit.h>
 @interface PriceView : UITableViewCell
 {
-    //Subview declaration comes here
+	UIView *_arrow;
+	UILabel *_titleLabel;
+	UILabel *_priceLabel;
+	UIView *_priceBarImageView;
+	UIView *_backgroundView;
+
 }
 @end
