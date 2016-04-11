9e22fbc7510c46c107771d9fa5c7adebe4b73779
Peter Wiesner <peter.wiesner@distinction.hu>
2016-04-11 20:28:27 +0200

    Added addSubviews

--- Generated/PriceView.m
+++ Generated/PriceView.m
@@ -5,7 +5,14 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if(self)
     {
-      //Subview initialization comes here
+//Subview initialization comes here
+
+	[self addSubview:_arrow];
+	[self addSubview:_titleLabel];
+	[self addSubview:_priceLabel];
+	[self addSubview:_priceBarImageView];
+	[self addSubview:_backgroundView];
+
     }
     return self;
 }
@@ -13,11 +20,11 @@
 {
     [super layoutSubviews];
 
-	[_arrow setFrame:CGRectMake(299,22,5,10)]
-	[_titleLabel setFrame:CGRectMake(9,18,43,17)]
-	[_priceLabel setFrame:CGRectMake(244,19,28,17)]
-	[_priceBarImageView setFrame:CGRectMake(60,23,173,9)]
-	[_backgroundView setFrame:CGRectMake(0,1,320,50)]
+	[_arrow setFrame:CGRectMake(299,22,5,10)];
+	[_titleLabel setFrame:CGRectMake(9,18,43,17)];
+	[_priceLabel setFrame:CGRectMake(244,19,28,17)];
+	[_priceBarImageView setFrame:CGRectMake(60,23,173,9)];
+	[_backgroundView setFrame:CGRectMake(0,1,320,50)];
 
 }
 @end
