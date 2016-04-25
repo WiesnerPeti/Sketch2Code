fd9abca8b8e5064971ac90aea9be4ff48008e44c
Peter Wiesner <peter.wiesner@distinction.hu>
2016-04-25 23:06:18 +0200

    Added Editor Sketch plugin to edit userInfos on MSLayer subclasses
    Updated deploy bash script to manage files to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-sudo cp -f "./sketch2Code.sketchplugin" ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
-echo "sketch2Code.sketchplugin deployed"
 
-sudo cp -f "./Common.snippet" ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
-echo "Common.snippet deployed"
+echo "I need to write to ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins"
 
-sudo cp -f "./UITableViewCell.snippet" ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
-echo "UITableViewCell.snippet deployed"
-
-sudo cp -f "./view.map" ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
-echo "view.map deployed"
+for file in "sketch2Code.sketchplugin" "Editor.sketchplugin" "Common.snippet" "UITableViewCell.snippet" "view.map";
+do
+  filePath="./$file"
+  sudo cp -f $filePath ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
+  echo "$file deployed"
+done
