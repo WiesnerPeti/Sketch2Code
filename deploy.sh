08e59175ca4a7b8ca22890b2fce47a812c1f56ef
Peter Wiesner <peter.wiesner@skyscanner.net>
2016-07-03 17:29:13 +0200

    Improved on deploy script, easier expendable

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,32 @@
 
 echo "I need to write to ~/Library/Application\ Support"
 
-for file in "sketch2Code.sketchplugin" "runtime.js" "io.js" "common.js" "logger.js" "Editor.sketchplugin" "Common.snippet" "UITableViewCell.snippet" "UIView.snippet" "view.map";
+pluginFiles=(
+	"sketch2Code.sketchplugin"
+	"runtime.js"
+	"io.js"
+	"common.js"
+	"logger.js"
+	"Editor.sketchplugin"
+	"Common.snippet"
+	"UITableViewCell.snippet"
+	"UIView.snippet"
+	"view.map"
+	)
+
+for file in "${pluginFiles[@]}";
 do
   filePath="./$file"
   sudo cp -f $filePath ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
   echo "$file deployed"
 done
 
-for file in "EditorPlugin.aslquery" "Sketch2CodePlugin.aslquery";
+consoleQueryFiles=(
+	"EditorPlugin.aslquery"
+	"Sketch2CodePlugin.aslquery"
+	)
+
+for file in "${consoleQueryFiles[@]}";
 do
   filePath="./$file"
   sudo cp -f $filePath ~/Library/Application\ Support/Console/ASLQueries
